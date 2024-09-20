afaf6c1d4da89a5880a887c916034e4803454437
Darlan Oliveira <feitt.c6@gmail.com>
2024-09-20 17:49:23 -0300

    feat(main): :sparkles: add title func

---
+++ generator.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"io"
+	"net/http"
+	"regexp"
+)
+
+// <-chan - canl somente leitura
+func title(urls ...string) <-chan string {
+	c := make(chan string)
+	for _, url := range urls {
+		go func(url string) {
+			resp, _ := http.Get(url)
+			defer resp.Body.Close()
+			html, _ := io.ReadAll(resp.Body)
+			r, _ := regexp.Compile("<title>(.*?)<\\/title>")
+			c <- r.FindStringSubmatch(string(html))[0]
+		}(url)
+	}
+	return c
+}
